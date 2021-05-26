88d715084d870c558cf13a0365cacca0713f07f0
Aiste Neciunaite <a.neciunaite@gmail.com>
2021-05-26 20:49:13 +0300

    Add PictureCard component

---
+++ src/components/PictureCard/PictureCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './style.scss'
+import CardBody from '../CardBody'
+import Button from '../Button'
+
+interface Props {
+  url: string
+  author: string
+  name: string
+}
+
+export default function PictureCard({ url, author, name }: Props) {
+  return (
+    <div className="PictureCard">
+      <img src={url} alt={name} />
+      <div className="PictureCard__overlay">
+        <CardBody title={name} subtitle={author} />
+        <Button onClick={() => {}}>Favorite</Button>
+      </div>
+    </div>
+  )
+}
