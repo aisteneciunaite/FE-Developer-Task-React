ec64349dc41eb8f7ded98a430d027720d64d7375
Aiste Neciunaite <a.neciunaite@gmail.com>
2021-05-26 20:48:46 +0300

    Add CardBody component

---
+++ src/components/CardBody/CardBody.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.scss'
+
+interface Props {
+  title: string
+  subtitle: string
+}
+
+export default function CardBody({ title, subtitle }: Props) {
+  return (
+    <div className="CardBody">
+      <h3>{title}</h3>
+      <hr />
+      <p>
+        <i>{subtitle}</i>
+      </p>
+    </div>
+  )
+}
