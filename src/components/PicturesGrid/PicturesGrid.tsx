63e6cccc8dbe7a4305414b813d1c155bfdaf5593
Aiste Neciunaite <a.neciunaite@gmail.com>
2021-05-27 08:07:10 +0300

    Add pictures grid component

---
+++ src/components/PicturesGrid/PicturesGrid.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PictureCard from '../PictureCard'
+import './PicturesGrid.scss'
+// TODO: delete this
+import a from '../../mockImages.json'
+
+export default function PicturesGrid() {
+  return (
+    <div className="PicturesGrid">
+      {/* TODO: add fetch pictures */}
+      {a.map((i, index) => (
+        <PictureCard {...i} key={index} />
+      ))}
+    </div>
+  )
+}
