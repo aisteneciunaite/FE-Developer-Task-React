f3ec5f4b5168fa361aad79a12208c32d087374d1
Aiste Neciunaite <a.neciunaite@gmail.com>
2021-05-29 13:39:03 +0300

    Implement infinate scroll

--- src/app/components/PicturesGrid/PicturesGrid.tsx
+++ src/app/components/PicturesGrid/PicturesGrid.tsx
@@ -2,17 +2,31 @@ import React from 'react'
 import PictureCard from '../PictureCard'
 import './PicturesGrid.scss'
 import useContent from '../../hooks/useContent'
+import InfiniteScroll from 'react-infinite-scroller'
 
 export default function PicturesGrid() {
-  const { content } = useContent()
+  const { content, hasNextPage, fetchContent, loading } = useContent()
+
+  const loadMore = async (page: number) => {
+    !loading && (await fetchContent(page))
+  }
+
   return (
-    <div className="PicturesGrid">
+    <InfiniteScroll
+      className="PicturesGrid"
+      pageStart={0}
+      loadMore={loadMore}
+      hasMore={hasNextPage}
+      initialLoad={true}
+      // TODO add nicer loader component
+      loader={<div key={0}>Loading ...</div>}
+    >
       {content.map((i, index) => {
         const { user } = i
         const author = `${user?.first_name || ''} ${user?.last_name || ''}`
         return (
           <PictureCard
-            url={i?.urls?.small}
+            url={i.urls.small}
             author={author}
             alt_text={i.alt_description}
             name={i.description || i.alt_description}
@@ -20,6 +34,6 @@ export default function PicturesGrid() {
           />
         )
       })}
-    </div>
+    </InfiniteScroll>
   )
 }
--- src/app/hooks/useContent.ts
+++ src/app/hooks/useContent.ts
@@ -1,34 +1,36 @@
-import { useCallback, useState, useEffect } from 'react'
+import { useCallback, useState } from 'react'
 import unsplash from '../../unsplash'
 import { Basic } from 'unsplash-js/dist/methods/photos/types'
 
 const useContent = () => {
   const [content, setContent] = useState([] as Basic[])
   const [loading, setLoading] = useState(false)
+  const [hasNextPage, setHasNextPage] = useState(true)
 
-  const fetchContent = useCallback(async () => {
+  const fetchContent = useCallback(async (page) => {
     setLoading(true)
-    try {
-      const res = await unsplash.search.getPhotos({
-        query: 'featured',
-        page: 1,
-        perPage: 10,
-        orientation: 'landscape',
-      })
-      if (res.status !== 200) throw new Error('server error')
-      setContent(res.response?.results || [])
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
-    }
+    setTimeout(async () => {
+      try {
+        const res = await unsplash.search.getPhotos({
+          query: 'featured',
+          page,
+          perPage: 30,
+          orientation: 'landscape',
+        })
+        if (res.status !== 200 || !res.response)
+          throw new Error('response error')
+        setContent((content) => [...content, ...res.response.results])
+        setHasNextPage(res.response.total_pages > page)
+      } catch (error) {
+        setHasNextPage(false)
+        alert(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }, 500)
   }, [])
 
-  useEffect(() => {
-    fetchContent()
-  }, [fetchContent])
-
-  return { content, loading }
+  return { content, loading, hasNextPage, fetchContent }
 }
 
 export default useContent
